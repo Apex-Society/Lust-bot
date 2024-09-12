d7fee0c86817fac8edb2e3b276079f772de01272
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-09-12 07:52:36 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,17 +3,18 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "5758713974"
-    sudo_users = ["5758713974", "7011990425", "6264760837", "5527022252", "6384484443", "1850686769"]
-    GROUP_ID = "-1002041586214"
-    TOKEN = "7371780430:AAH3wBjiu4nRirz-A5U_L7beK97TmWicQSY"
-    mongo_url = "mongodb+srv://vinamra523:duXNC3pKUddFNb1p@newbhainew.3ioyzuw.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
-    SUPPORT_CHAT = "lustsupport"
-    UPDATE_CHAT = "lustxUpdate"
-    BOT_USERNAME = "lustXcatcherr_robot"
-    CHARA_CHANNEL_ID = "-1002023474262"
-    api_id = "20457610"
-    api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
+    sudo_users = "5758713974" ,"6679467894", "1837623818", "6916220465", "6494814225", "5079126270", "7350197183", "5138772561", "1440712150"
+    GROUP_ID = -1002190080867
+    TOKEN = "6656009300:AAGgaIl9uJ438ZVpLP1R5k5_fs3piACUqiU"
+    mongo_url = "mongodb+srv://tiwarireeta004:peqxLEd36RAg7ors@cluster0.furypd3.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL =["https://te.legra.ph/file/161e4748aff1915cf21f0.jpg", "https://te.legra.ph/file/0c0dd1968dfb3963275dd.jpg"]
+    SUPPORT_CHAT = "ApeX_Chats"
+    UPDATE_CHAT = "ApeX_Chats"
+    BOT_USERNAME = "GUESSEM_ALL_BOT"
+    CHARA_CHANNEL_ID = "-1002190080867"
+    api_id = 24074986
+    api_hash = "f4f6272a85d0e50e39a24cb378be118d"
+    
 
     
 class Production(Config):
